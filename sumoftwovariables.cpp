9e6ebb8941e99540d4cf3345c75251aaba67e46a
Jayantyadav26 <95078136+Jayantyadav26@users.noreply.github.com>
2022-02-02 21:17:48 +0530

    Add files via upload

---
+++ sumoftwovariables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{int x,y,sum;
+cout<<"enter a number: ";
+cin>> x;
+cout<<"enter another number: ";
+cin>> y;
+sum= x+y;
+cout<<"Sum of two number: " << sum;
+return 0;}
