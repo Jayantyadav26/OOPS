21affa04ab54680ea6492f14d9d8f0e0ff71c301
Jayantyadav26 <95078136+Jayantyadav26@users.noreply.github.com>
2022-02-06 20:21:13 +0530

    Add files via upload
    bubble chart in c++ used in data structure and algorithm.

---
+++ bubble chart.cpp
@@ -0,0 +1,37 @@
+ #include <iostream>
+ using namespace std;
+ 
+ void bubblesort(int a[])
+ {for(int i=0; i<5;i++){
+ 	for (int j=0;j<(5-i-1); j++)
+ 	{if(a[j]>a[j+1]){
+ 		int temp=a[j];
+ 		a[j]=a[j+1];
+ 		a[j+1]=temp;
+	 }
+	 }
+ }
+ }
+ int main()
+ { int myarray[5];
+ cout<<"enter 5 integers in any order: "<<endl;
+ for(int i=0;i<5;i++)
+ {cin>>myarray[i];
+ 
+ }
+ 
+ bubblesort(myarray);
+ 
+ cout<<endl<<"after sorting: "<<endl;
+ for(int i=0;i<5;i++)
+ {cout<<myarray[i]<<" ";
+ }
+ 	 	
+return 0;
+ }
+
+
+
+ 
+
+ 
